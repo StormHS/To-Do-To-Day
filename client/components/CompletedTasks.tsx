25efb0963154a66500bc010b89db5f33744c7d5a
Michael Jansen <michael.stradiv@gmail.com>
2023-07-03 14:04:00 +1200

    complete undo function

--- client/components/CompletedTasks.tsx
+++ client/components/CompletedTasks.tsx
@@ -1,8 +1,50 @@
-import { getTasks } from '../apis/tasks'
-import { useQuery } from '@tanstack/react-query'
+import { getTasks, updateCompletion } from '../apis/tasks'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { useEffect, useState } from 'react'
+import { TaskRecord } from '../../models/task'
+import NavBar from './NavBar'
+import { faUndo } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function CompletedTasks() {
   const { data: tasks, error, isLoading } = useQuery(['tasks'], getTasks)
+  const [editedTasks, setEditedTasks] = useState<TaskRecord[] | undefined>(undefined)
+  const queryClient = useQueryClient()
+  const [over, setOver] = useState(false)
+  
+  useEffect(() => {
+    if (tasks && !editedTasks) {
+      setEditedTasks(tasks)
+    }
+  }, [tasks, editedTasks])
+
+  const completeTaskMutation = useMutation(updateCompletion, {
+    onSuccess: async () => {
+      queryClient.invalidateQueries(['tasks'])
+    },
+  })
+
+  const handleTaskComlpete = async (taskId: number) => {
+    const updatedTasks = tasks?.map((task) => {
+      if (task.id === taskId) {
+        return {
+          ...task, 
+          completed: !task.completed
+        }
+      }
+      return task
+    })
+  setEditedTasks(updatedTasks)
+
+  const taskToUpdate =  tasks?.find((task) => task.id === taskId)
+  console.log(taskToUpdate)
+  if (taskToUpdate)
+  try {
+    await completeTaskMutation.mutate({...taskToUpdate, completed: false})
+  } catch (error) {
+    console.error('Unable to update task status!')
+  }
+  }
 
   if (error) {
     if (error instanceof Error) {
@@ -22,6 +64,7 @@ export default function CompletedTasks() {
   return (
     <section>
       <div>
+        <NavBar />
         <h1>What You Did Today</h1>
         <div className="container">
           <div className="img-container">
@@ -31,16 +74,35 @@ export default function CompletedTasks() {
               alt="Little animal"
             />
           </div>
-          {results.map(({ id, name, description }) => {
-            return (
-              <ul className="listFlex" key={id}>
-                <li>Task: {name}</li>
-                <li>Notes: {description}</li>
-              </ul>
-            )
+          <ul className="listFlex">
+            {results.map(({ id, name, description, completed }) => {
+              return (
+                <li key={id} style={{ listStyleType: 'none'}}>
+                <label style={{ 
+                    display: 'flex', 
+                    alignItems: 'center' }}   
+                    onMouseOver={() => setOver(true)}
+                    onMouseLeave={() => setOver(false)}
+                    onFocus={() => setOver(true)}
+                    >
+                    <FontAwesomeIcon icon={faUndo} style={over ? { color: "red" } : {}} />
+                  <input 
+                    type="checkbox"
+                  
+                    style={{ marginRight: '0.5rem', visibility: "hidden"}}
+                    checked={completed}
+                    onChange={() => handleTaskComlpete(id)}
+                  />
+                </label>
+                <p>TaskRecord: {name}</p>
+                <p>Notes: {description}</p>
+              </li>
+              )
           })}
+          </ul>
         </div>
       </div>
     </section>
   )
 }
+
--- client/components/NavBar.tsx
+++ client/components/NavBar.tsx
@@ -11,27 +11,29 @@ export default function NavBar() {
   return (
     <div className="nav-bar">
       <table>
-        <tr>
-          <th>
-            <a className="nav-bar-buttons" href="/">
-              To Do
-            </a>
-          </th>
-          <th>
-            <a className="nav-bar-buttons" href="/">
-              Done
-            </a>
-          </th>
-          <th>
-            {' '}
-            <IfAuthenticated>
-              <button onClick={handleSignOut} className="sign-out-button">
-                Sign out
-              </button>
-              {/* {user && <p>Signed in as: {user?.nickname}</p>} */}
-            </IfAuthenticated>
-          </th>
-        </tr>
+        <tbody>
+          <tr>
+            <th>
+              <a className="nav-bar-buttons" href="/">
+                To Do
+              </a>
+            </th>
+            <th>
+              <a className="nav-bar-buttons" href="/completed">
+                Done
+              </a>
+            </th>
+            <th>
+              {' '}
+              <IfAuthenticated>
+                <button onClick={handleSignOut} className="sign-out-button">
+                  Sign out
+                </button>
+                {/* {user && <p>Signed in as: {user?.nickname}</p>} */}
+              </IfAuthenticated>
+            </th>
+          </tr>
+        </tbody>
       </table>
     </div>
   )
