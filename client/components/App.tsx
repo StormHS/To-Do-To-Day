441e7b41ae7f095dbdfa47fa48126ef8fbacfad9
Kera Gifkins <keragifkins@outlook.co.nz>
2023-07-04 09:52:59 +1200

    homework level achieved

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { IfAuthenticated, IfNotAuthenticated } from './Authenticated'
 import DayTaskList from './DayTaskList'
 import CompletedTasks from './CompletedTasks'
-import NavBar from './NavBar'      
+import NavBar from './NavBar'
 import Home from './Home'
 
 export default function App() {
@@ -16,7 +16,6 @@ export default function App() {
         <div>
           <IfAuthenticated>
             <Routes>
-              {/* remove path/todo and make the element of todo be the element of path / */}
               <Route path="/" element={<DayTaskList />} />
               <Route path="/completed" element={<CompletedTasks />} />
             </Routes>
--- client/components/CompletedTasks.tsx
+++ client/components/CompletedTasks.tsx
@@ -1,4 +1,3 @@
-
 import { getTasks, updateCompletion } from '../apis/tasks'
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import { useEffect, useState } from 'react'
@@ -6,15 +5,23 @@ import { TaskRecord } from '../../models/task'
 import { faUndo } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useAuth0 } from '@auth0/auth0-react'
-import NavBar from './NavBar'
+import { IfAuthenticated, IfNotAuthenticated } from './Authenticated'
+import Home from './Home'
+import Popup from 'reactjs-popup'
 
 export default function CompletedTasks() {
   const auth = useAuth0()
-  const { data: tasks, error, isLoading } = useQuery(['tasks'], () => getTasks(auth))
-  const [editedTasks, setEditedTasks] = useState<TaskRecord[] | undefined>(undefined)
+  const {
+    data: tasks,
+    error,
+    isLoading,
+  } = useQuery(['tasks'], () => getTasks(auth))
+  const [editedTasks, setEditedTasks] = useState<TaskRecord[] | undefined>(
+    undefined
+  )
   const queryClient = useQueryClient()
   const [over, setOver] = useState(false)
-  
+
   useEffect(() => {
     if (tasks && !editedTasks) {
       setEditedTasks(tasks)
@@ -31,22 +38,22 @@ export default function CompletedTasks() {
     const updatedTasks = tasks?.map((task) => {
       if (task.id === taskId) {
         return {
-          ...task, 
-          completed: !task.completed
+          ...task,
+          completed: !task.completed,
         }
       }
       return task
     })
-  setEditedTasks(updatedTasks)
+    setEditedTasks(updatedTasks)
 
-  const taskToUpdate =  tasks?.find((task) => task.id === taskId)
-  console.log(taskToUpdate)
-  if (taskToUpdate)
-  try {
-    await completeTaskMutation.mutate({...taskToUpdate, completed: false})
-  } catch (error) {
-    console.error('Unable to update task status!')
-  }
+    const taskToUpdate = tasks?.find((task) => task.id === taskId)
+    console.log(taskToUpdate)
+    if (taskToUpdate)
+      try {
+        await completeTaskMutation.mutate({ ...taskToUpdate, completed: false })
+      } catch (error) {
+        console.error('Unable to update task status!')
+      }
   }
 
   if (error) {
@@ -60,52 +67,63 @@ export default function CompletedTasks() {
   if (!tasks || isLoading) {
     return <div>Loading...</div>
   }
-  
-  const results = tasks.filter((task) => task.completed)
 
-  // todo: if authenticated
+  const results = tasks.filter((task) => task.completed)
 
   return (
     <section>
-      <div>
-        <h1>What You Did Today</h1>
-        <div className="container">
-          <div className="img-container">
-            <img
-              className="imgFlex"
-              src="../../images/companion.png"
-              alt="Little animal"
-            />
-          </div>
+      <IfAuthenticated>
+        <div>
+          <h1>What You Did Today</h1>
+          <div className="container">
+            <div className="img-container">
+              <img
+                className="imgFlex"
+                src="../../images/companion.png"
+                alt="Little animal"
+              />
+            </div>
 
-          <ul className="listFlex">
-            {results.map(({ id, name, description, completed }) => {
-              return (
-                <li key={id} style={{ listStyleType: 'none'}}>
-                <label style={{ 
-                    display: 'flex', 
-                    alignItems: 'center' }}   
-                    onMouseOver={() => setOver(true)}
-                    onMouseLeave={() => setOver(false)}
-                    onFocus={() => setOver(true)}
+            <ul className="listFlex">
+              {results.map(({ id, name, description, completed }) => {
+                return (
+                  <li key={id} style={{ listStyleType: 'none' }}>
+                    <label
+                      style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                      }}
+                      onMouseOver={() => setOver(true)}
+                      onMouseLeave={() => setOver(false)}
+                      onFocus={() => setOver(true)}
                     >
-                    <FontAwesomeIcon icon={faUndo} style={over ? { color: "red" } : {}} />
-                  <input 
-                    type="checkbox"
-                    style={{ marginRight: '0.5rem', visibility: "hidden"}}
-                    checked={completed}
-                    onChange={() => handleTaskComlpete(id)}
-                  />
-                </label>
-                <h2>Task: {name}</h2>
-                <p>Notes: {description}</p>
-              </li>
-              )
-          })}
-          </ul>
+                      <FontAwesomeIcon
+                        icon={faUndo}
+                        style={over ? { color: 'red' } : {}}
+                      />
+                      <input
+                        type="checkbox"
+                        style={{ marginRight: '0.5rem', visibility: 'hidden' }}
+                        checked={completed}
+                        onChange={() => handleTaskComlpete(id)}
+                      />
+                    </label>
+                    <Popup
+                      trigger={<button className="task-written">{name}</button>}
+                      position="bottom center"
+                    >
+                      <p className="notes">Notes - {description}</p>
+                    </Popup>
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
         </div>
-      </div>
+      </IfAuthenticated>
+      <IfNotAuthenticated>
+        <Home />
+      </IfNotAuthenticated>
     </section>
   )
 }
-
