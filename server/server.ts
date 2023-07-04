ff969721cda15a1e1db052d9644128227952ec19
Euan Cunningham <euancc06@gmail.com>
2023-07-05 10:34:38 +1200

    edit server file

--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,17 @@
 import express from 'express'
-import { join } from 'node:path'
 
 import taskRoutes from './routes/tasks'
 
 const server = express()
 
 server.use(express.json())
-server.use(express.static(join(__dirname, 'public')))
 
 server.use('/api/v1/tasks', taskRoutes)
+if (process.env.NODE_ENV === 'production') {
+  server.use('/assets', express.static('/app/dist/assets'))
+  server.get('*', (req, res) => {
+    res.sendFile('/app/dist/index.html')
+  })
+}
 
 export default server
